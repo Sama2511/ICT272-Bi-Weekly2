9096372a303036d3d9c3c14776765d5adce7c70b
Oussama <Hassouneoussama8@gmail.com>
2025-08-07 18:08:44 +1000

    Upload to Git

---
+++ tutorial5/Controllers/CampusController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using tutorial5.Models;
+
+namespace tutorial5.Controllers
+{
+    public class CampusController : Controller
+    {
+        // GET: Campus
+         public string Overview()
+        {
+            return "This page provides an overview of our campus.";
+        }
+
+        // GET: Campus/Locations
+        public ActionResult Locations()
+        {
+            var campuses = new List<Campus>
+            {
+                new Campus { Id = 1, Name = "Main Campus", Location = "123 Main St, Springfield" },
+                new Campus { Id = 2, Name = "West Campus", Location = "456 Elm St, Shelbyville" },
+                new Campus { Id = 3, Name = "Downtown Campus", Location = "789 Oak St, Capital City" }
+            };
+
+            return View(campuses);
+        }
+    }
+}
---
+++ tutorial5/Controllers/SubjectController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace tutorial5.Controllers
+{
+    public class SubjectController : Controller
+    {
+        // GET: Subject
+        public string Index()
+        {
+            return "This page to show the subject homepage";
+        }
+
+        public string Subjects()
+        {
+            return "This page is to show the list of subjects that the student is enrolled in";
+        }
+
+        public string Details(string subjectId)
+        {
+            return HttpUtility.HtmlEncode("Subject Code: " + subjectId);
+        }
+
+        public ActionResult Edit(int subjectId)
+        {
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int subjectId)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
